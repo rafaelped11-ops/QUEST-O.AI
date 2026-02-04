098cdc48ed6d8c1004d73b5290900b23f1ada55e
Rafael Salesx <Rafaelped11@gmail.com>
2026-02-04 02:32:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6005948775-85a76",
+  "appId": "1:523529508311:web:0711b984115f6612bda792",
+  "apiKey": "AIzaSyDTlxWirYjeNavt84XBRne7Qwie_z48iGM",
+  "authDomain": "studio-6005948775-85a76.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "523529508311"
+};
